84a78a252f67acbb13e14a2f3180240194ec0cc1
mohammedelkouche <mohammed0elkouche@gmail.com>
2025-03-16 17:13:33 +0000

    feature : add JWT Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,9 @@
-const router = require("express").Router()
-const User = require ("../models/user")
+const router = require("express").Router();
+const User = require ("../models/user");
+const bcrypt = require ("bcryptjs");
+const jwt = require ("jsonwebtoken");
+require("dotenv").config();
+
 
 // SIGN IN API  
 
@@ -19,10 +23,12 @@ router.post("/sign-in",async(req, res) => {
             return res.status(400).json({message : "username should have atleast 4 characters"})
         if (existingeEmail)
             return res.status(400).json({message : "email alreadi existe"})
+        const hashPass = await bcrypt.hash(req.body.password, 10); // relation await with 10 ?
+
         const newUser = new User ( {
             username : req.body.username,
             email : req.body.email,
-            password : req.body.password,
+            password : hashPass,
         });
         await newUser.save();
         return res.status(200).json({message : "SignIn succesfully "})
@@ -30,6 +36,26 @@ router.post("/sign-in",async(req, res) => {
         console.log("the error message => ",error);
         return res.status(400).json({message : "Internal Server Error"});
     }
+});
+
+// login
+
+router.get("/log-in", async(req, res) => {
+    const {username, password} = req.body;
+    const existingUser = await User.findOne({username : username});
+    if (!existingUser)
+        return res.status(400).json({message : "invalide credentials"})
+    bcrypt.compare(password, existingUser.password, (err,data) => {
+        if (data)
+        {
+            const authClaims = [{name:username}, {jit : jwt.sign({}, process.env.JWT_SECRET)}];
+            const token = jwt.sign({authClaims}, process.env.JWT_SECRET, {expiresIn:"2d"});
+            res.status(200).json({id : existingUser._id, token : token});
+        }else
+        {
+            return res.status(400).json({message : "invalide credentials"})
+        }
+    } )
 });
 
 module.exports = router;
